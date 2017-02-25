52226bb19c24e12e050f5e0a261649bf145cbfb8
ViolaMary <janetviolamary@gmail.com>
2017-02-25 16:28:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,68 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var ArticleOne={
+    title:'Article One | Janet',
+    heading:'Article One',
+    date: 'September 5 2017',
+    content: `<p>This is my content for article.This is my content for article.This is my content for article.
+                   This is my content for article.This is my content for article.This is my content for article.
+                   This is my content for article.This is.my content for article.This is my content for article.
+                   This is my content for article.This is my content for article.This is my content for article.
+                </p>
+            
+            
+            <p>This is my content for article.This is my content for article.This is my content for article.
+                   This is my content for article.This is my content for article.This is my content for article.
+                   This is my content for article.This is.my content for article.This is my content for article.
+                   This is my content for article.This is my content for article.This is my content for article.
+                </p>
+            
+                <p>This is my content for article.This is my content for article.This is my content for article.
+                   This is my content for article.This is my content for article.This is my content for article.
+                   This is my content for article.This is.my content for article.This is my content for article.
+                   This is my content for article.This is my content for article.This is my content for article.
+                </p>`
+};
+function createTemplate(data){
+var title=data.title;
+var heading=data.heading;
+var date=data.date;
+var content=data.content;
+var htmltemplate=`
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name= "viewport" content="width-device-width ,initial-scale-1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class ="container">
+            
+                <div>
+                    <a href='/'>Home </a>
+                </div>
+                <hr/>
+                <div>
+                    <h3>${heading}</h3>
+                </div>
+                <div>
+                    <h4>${date}</h4>
+                </div>
+                <div>
+                    ${content}
+                </div>
+            <div>
+        </body>
+    </html>
+    `;
+    return htmltemplate;
+}
+
 app.get('/article-one',function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(ArticleOne);
 });
 
 app.get('/article-two',function(req, res){
